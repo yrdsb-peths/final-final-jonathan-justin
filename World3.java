6a7b3eeb23a575f0521cd35d8f8632b46a76eee4
just1ngli <justinli99.ca@gmail.com>
2026-01-12 14:12:35 -0500

    world 3 base level done could have improvements

--- World3.java
+++ World3.java
@@ -23,40 +23,75 @@ public class World3 extends World
     
     private void prepare(Player player)
     {
-        addObject(player, 50, 50);
+        addObject(player, 50, 400);
+        addObject(new Coin(false), 24, 472);
+        addObject(new Portal(), 504, 230);
+        
         setBackground("world3Outside.png");
         for(int i = 0; i < 4; i++)addObject(new platforms1(), 8, 424 + i * 16);
         for(int i = 0; i < 4; i++)addObject(new platforms1(), 8 + i * 16, 488);
+        addObject(new platforms2(), 8, 198);
         
         for(int i = 0; i < 2; i++)addObject(new platforms1(), 24, 424 + i * 16);
-    
+        addObject(new platforms2(), 24, 214);
+        
         
         for(int i = 0; i < 9; i++)addObject(new platforms1(), 40 + i * 16, 424);
         
+        for(int i = 0; i < 2; i++)addObject(new platforms2(), 56 + i * 16, 262);
+        addObject(new platforms1(), 56, 152);
+        
         for(int i = 0; i < 28; i++)addObject(new platforms1(), 72 + i * 16, 504);
         
+        for(int i = 0; i < 2; i++)addObject(new platforms2(), 136 + i * 16, 294);
+        addObject(new platforms2(), 136, 150);
+        
         for(int i = 0; i < 3; i++)addObject(new platforms1(), 152 + i * 16, 408);
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 152, 136 + i * 16);
         
         for(int i = 0; i < 2; i++)addObject(new platforms1(), 184 + i * 16, 392);
         addObject(new platforms1(), 184, 488);
+        addObject(new platforms2(), 184, 86);
         
         for(int i = 0; i < 6; i++)addObject(new platforms1(), 200 + i * 16, 472);
-        
         for(int i = 0; i < 4; i++)addObject(new platforms1(), 200 + i * 16, 376);
         for(int i = 0; i < 2; i++)for(int x = 0; x < 2; x++)addObject(new platforms1(), 216 + x * 16, 424 + i * 16);
         
+        addObject(new platforms1(), 216, 104);
+        
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 248, 264 - i * 16);
+        
         for(int i = 0; i < 2; i++)addObject(new platforms1(), 264 + i * 16, 360);
         for(int i = 0; i < 2; i++)addObject(new platforms1(), 264 - i * 16, 408);
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 264, 280 - i * 16);
         
         for(int i = 0; i < 3; i++)addObject(new platforms1(), 280 + i * 16, 456);
         for(int i = 0; i < 2; i++)addObject(new platforms1(), 280, 376 + i * 16);
-            
-        addObject(new platforms1(), 328, 440);
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 280, 296 - i * 16);
+        for(int i = 0; i < 2; i++)addObject(new platforms2(), 280 + i * 16, 166);
         
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 296, 312 - i * 16);
+        
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 312, 328 - i * 16);
+
+        addObject(new platforms1(), 328, 440);
+        addObject(new platforms1(), 328, 344);
+
         for(int i = 0; i < 3; i++)addObject(new platforms1(), 344, 472 - i * 16);
+        
         for(int i = 0; i < 3; i++)addObject(new platforms1(), 360 + i * 16, 488);
         
-        addObject(new platforms2(), 408, 406);
+        for(int i = 0; i < 2; i++)addObject(new platforms2(), 392 - i * 16, 166);
         
+        addObject(new platforms2(), 408, 406);
+        addObject(new platforms2(), 408, 118);
+        
+        addObject(new platforms1(), 424, 312);
+        addObject(new platforms2(), 424, 70);
+        
+        for(int i = 0; i < 2; i++)addObject(new platforms2(), 472 + i * 16, 374);
+        
+        for(int i = 0; i < 4; i++)addObject(new platforms1(), 504, 376 - i * 16);
+        addObject(new platforms2(), 504, 246);
     }
 }
