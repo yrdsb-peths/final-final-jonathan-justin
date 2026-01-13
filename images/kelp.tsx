e353e309205e1eb480a3d07a581c44b51f43f1db
just1ngli <justinli99.ca@gmail.com>
2026-01-13 18:02:57 -0500

    added world4.png

---
+++ images/kelp.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="kelp" tilewidth="16" tileheight="16" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="kelp/kelp0.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="kelp/kelp1.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="kelp/kelp2.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="kelp/kelp3.png" width="16" height="16"/>
+ </tile>
+</tileset>
