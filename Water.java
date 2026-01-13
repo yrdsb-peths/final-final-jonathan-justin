f0adfaccb518b1ba87bd07eaccd529c90eafceba
xujonathan123 <xujonathan123@gmail.com>
2026-01-13 17:28:09 -0500

    swimming works

---
+++ Water.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Water here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Water extends Actor
+{
+    /**
+     * Act - do whatever the Water wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        GreenfootImage image = new GreenfootImage(16, 16);
+        image.setColor(Color.ORANGE);
+        image.fill();
+        setImage(image);
+        getImage().setTransparency(0);
+    }
+}
