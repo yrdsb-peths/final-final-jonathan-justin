909a3f573826b1e560000b6735c801e312fb07f5
just1ngli <justinli99.ca@gmail.com>
2026-01-13 21:17:29 -0500

    world5 WIP

--- World5.java
+++ World5.java
@@ -19,15 +19,99 @@ public class World5 extends World
         super(785, 448, 1, false); 
         setBackground("world5.png");
         
-        
+        addObject(player, 536, 50);
         prepare(player);
     }
     
     private void prepare(Player player)
     {
-        addObject(player, 520, 288);
-        for(int i = 0; i < 5; i++)addObject(new platforms1(), 520, 296 + i * 16);
+        //addObject(player, 536, 260);
+
+        
+        for(int i = 0; i < 5; i++)addObject(new platforms1(), 8 + i * 16, 56);
+        for(int i = 0; i < 7; i++)addObject(new platforms1(), 40, 152 + i * 16);
+        addObject(new platforms1(), 56, 136);
+        addObject(new platforms1(), 56, 264);
+        addObject(new platforms1(), 72, 280);
+        addObject(new platforms1(), 72, 120);
+        addObject(new platforms1(), 88, 296);
+        for(int i = 0; i < 12; i++)addObject(new platforms1(), 88 + i * 16, 72);
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 104 - i * 16, 104);
+        addObject(new platforms1(), 104, 312);
+        addObject(new platforms1(), 120, 88);
+        addObject(new platforms1(), 120, 328);
+        for(int i = 0; i < 3; i++)addObject(new platforms1(), 168 - i * 16, 344);
+        for(int i = 0; i < 8; i++)addObject(new platforms1(), 296 - i * 16, 360);
+        addObject(new platforms1(), 312, 344);
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 328, 312 + i * 16);
+        for(int i = 0; i < 10; i++)addObject(new platforms1(), 344, 152 + i * 16);
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 360, 184 - i * 16);
+        addObject(new platforms1(), 360, 312);
+        addObject(new platforms1(), 376, 328);
+        addObject(new platforms1(), 392, 344);
+        addObject(new platforms1(), 392, 104);
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 408, 104 + i * 16);
+        for(int i = 0; i < 4; i++)addObject(new platforms1(), 408, 232 + i * 16);
+        for(int i = 0; i < 13; i++)addObject(new platforms1(), 424, 104 + i * 16);
+        for(int i = 0; i < 7; i++)addObject(new platforms1(), 440, 104 + i * 16);
+        for(int i = 0; i < 4; i++)addObject(new platforms1(), 456 - i * 16, 360);
+        for(int i = 0; i < 15; i++)addObject(new platforms1(), 472 - i * 16, 88);
+        addObject(new platforms1(), 472, 344);
+        for(int i = 0; i < 6; i++)addObject(new platforms1(), 488, 248 + i * 16);
+        for(int i = 0; i < 7; i++)addObject(new platforms1(), 504, 136 + i * 16);
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 520, 408 + i * 16);
+        for(int i = 0; i < 14; i++)addObject(new platforms1(), 520, 296 - i * 16);
+        for(int i = 0; i < 6; i++)addObject(new platforms1(), 536, 312 + i * 16);
+        for(int i = 0; i < 2; i++)addObject(new platforms1(), 536, 88 + i * 16);
+        addObject(new platforms2(), 552, 342);
+        for(int i = 0; i < 2; i++)addObject(new platforms2(), 552 + i * 16, 84);
+        addObject(new platforms2(), 584, 310);
+        addObject(new platforms2(), 616, 150);
+        addObject(new platforms2(), 632, 198);
+        addObject(new platforms2(), 632, 102);
+        addObject(new platforms2(), 648, 310);
+        addObject(new platforms2(), 680, 214);
+        addObject(new platforms2(), 696, 294);
+        addObject(new platforms2(), 744, 230);
+        for(int i = 0; i < 2; i++)addObject(new platforms2(), 760 - i * 16, 278);
+        for(int i = 0; i < 6; i++)addObject(new platforms1(), 776, 280 + i * 16);
+        for(int y = 0; y < 2; y++)for(int i = 0; i < 14; i++)addObject(new platforms1(), 760 - i * 16, 392 + y * 16);
+        for(int i = 0; i < 16; i++)addObject(new platforms1(), 776 - i * 16, 440);
+        addWater();
     }
+    private void addWater()
+    {
+        for(int i = 0; i < 3; i++)addObject(new Water(), 56 + i * 16, 184);
+        for(int i = 0; i < 3; i++)addObject(new Water(), 56 + i * 16, 200);
+        for(int i = 0; i < 3; i++)addObject(new Water(), 56 + i * 16, 216);
+        for(int i = 0; i < 4; i++)addObject(new Water(), 56 + i * 16, 232);
+        for(int i = 0; i < 5; i++)addObject(new Water(), 56 + i * 16, 248);
+        for(int i = 0; i < 5; i++)addObject(new Water(), 72 + i * 16, 264);
+        for(int i = 0; i < 5; i++)addObject(new Water(), 88 + i * 16, 280);
+        for(int i = 0; i < 6; i++)addObject(new Water(), 104 + i * 16, 296);
+        for(int i = 0; i < 13; i++)addObject(new Water(), 120 + i * 16, 312);
+        for(int i = 0; i < 12; i++)addObject(new Water(), 136 + i * 16, 328);
+        for(int i = 0; i < 8; i++)addObject(new Water(), 184 + i * 16, 344);
+        for(int i = 0; i < 4; i++)addObject(new Water(), 280 + i * 16, 296);
+        for(int y = 0; y < 11; y++)for(int i = 0; i < 3; i++)addObject(new Water(), 296 + i * 16, 280 - y * 16);
+        for(int i = 0; i < 5; i++)addObject(new Water(), 312 + i * 16, 104);
+        for(int i = 0; i < 4; i++)addObject(new Water(), 344 + i * 16, 120);
+        for(int i = 0; i < 5; i++)addObject(new Water(), 344 + i * 16, 136);
+        for(int i = 0; i < 2; i++)addObject(new Water(), 360, 200 + i * 16);
+        for(int i = 0; i < 4; i++)addObject(new Water(), 360 + i * 16, 152);
+        for(int y = 0; y < 4; y++)for(int i = 0; i < 3; i++)addObject(new Water(), 360 + i * 16, 232 + y * 16);
+        for(int y = 0; y < 4; y++)for(int i = 0; i < 3; i++)addObject(new Water(), 376 + i * 16, 168 + y * 16);
+        for(int i = 0; i < 4; i++)addObject(new Water(), 360 + i * 16, 296);
+        for(int i = 0; i < 4; i++)addObject(new Water(), 376 + i * 16, 312);
+        for(int i = 0; i < 3; i++)addObject(new Water(), 392 + i * 16, 328);
+        for(int i = 0; i < 2; i++)addObject(new Water(), 408 + i * 16, 344);
+        for(int i = 0; i < 9; i++)addObject(new Water(), 440, 344 - i * 16);
+        for(int i = 0; i < 16; i++)addObject(new Water(), 456, 344 - i * 16);
+        for(int i = 0; i < 15; i++)addObject(new Water(), 472, 328 - i * 16);
+        for(int i = 0; i < 9; i++)addObject(new Water(), 488, 232 - i * 16);
+        for(int i = 0; i < 2; i++)addObject(new Water(), 504, 120 - i * 16);
+    }
+    
     private boolean resetting = false;
     public void act()
     {
